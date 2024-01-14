35c24f2ddae7157dd0e6829e4eeeac8a4c7523a4
rrvillareal23 <rrvillareal@arizona.edu>
2024-01-14 07:54:27 -0800

    Log in functionalities

--- website/views.py
+++ website/views.py
@@ -1,4 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+#login info
+#admin
+#password
 
 def home(request):
-    return render(request, 'home.html')
+    #check to see if loggin in
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        #authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in!")
+            return redirect('home')
+        else:
+            messages.success(request, "Error logging in, please try again...")
+            return redirect('home')
+        
+    else:
+        return render(request, 'home.html', {})
+    
+    return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    pass
