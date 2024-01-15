cfb1f7ff20fa206505707158de975a5ba8b99df8
rrvillareal23 <rrvillareal@arizona.edu>
2024-01-15 12:04:53 -0800

    Completed Registration form authentication and error checking

--- website/forms.py
+++ website/forms.py
@@ -1,12 +1,11 @@
-from typing import Self
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(label="", widget=forms.TextInput(attr={'class': 'form-control', 'placeholder': 'Email Address'}))
-    first_name = forms.CharField(label="",max_length=100, widget=forms.TextInput(attr={'class': 'form-control', 'placeholder': 'First Name'}))
-    last_name = forms.CharField(label="",max_length=100, widget=forms.TextInput(attr={'class': 'form-control', 'placeholder': 'Last Name'}))
+    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'}))
+    first_name = forms.CharField(label="",max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
+    last_name = forms.CharField(label="",max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
 
     class Meta:
         model = User
