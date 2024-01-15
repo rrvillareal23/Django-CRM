98e0f543385ee86359d4eb5f77c4310a6898eec3
rrvillareal23 <rrvillareal@arizona.edu>
2024-01-15 12:35:37 -0800

    Create and Display Records on Home Screen

--- website/views.py
+++ website/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 #login info
 #admin
 #password
 
 def home(request):
+    records = Record.objects.all()
+    
+    
     #check to see if loggin in
     if request.method == "POST":
         username = request.POST['username']
@@ -24,7 +28,7 @@ def home(request):
             return redirect('home')
         
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
     
 
 def logout_user(request):
