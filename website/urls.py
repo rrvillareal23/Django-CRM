3419115f9545026a97ef4c8ef7e51eb3965b6233
rrvillareal23 <rrvillareal@arizona.edu>
2024-01-16 13:34:58 -0800

    Added Record Html so you can click into specific customer record

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.home, name='home'),
     path("logout/", views.logout_user, name='logout'),
     path("register/", views.register_user, name='register'),
+    path("record/<int:pk>", views.customer_record, name='record'),
 
 ]
--- website/views.py
+++ website/views.py
@@ -54,4 +54,13 @@ def register_user(request):
     
     return render(request, 'register.html', {'form': form})
 
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        #Look up record
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, "You must be logged in to see that record!")
+        return redirect('home')
         
