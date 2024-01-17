f82777ac1039820c91c681a6c5ba2431cce3359c
rrvillareal23 <rrvillareal@arizona.edu>
2024-01-16 19:29:09 -0800

    Added delete button when inside record of customer

--- website/views.py
+++ website/views.py
@@ -64,3 +64,12 @@ def customer_record(request, pk):
         messages.success(request, "You must be logged in to see that record!")
         return redirect('home')
         
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record deleted successfully...")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in to Delete a Record!")
+        return redirect('home')
